26493df3c16f4fda167e9b7a288dba2ea06c0f5f
AnandC7github <122742595+AnandC7github@users.noreply.github.com>
2024-01-22 12:45:40 +0000

    returned back to the original code from the last commit the change made in '/home' or '/' being changed to '/homehomehome'.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def about_user(username):
 def about_page():
     return '<h1>About Page</h1>'
 
-@app.route('/homehomehome')
+@app.route('/home')
 def home_page():
     return render_template('home.html')
 
