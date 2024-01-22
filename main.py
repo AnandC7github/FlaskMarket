ab6e140878d126f44a11ef6cad0aff8e8bc0434e
AnandC7github <122742595+AnandC7github@users.noreply.github.com>
2024-01-22 11:57:49 +0000

    added jinja2 block for forloop to display the dictionary that had been included in main.py. also have added two buttons for more info and purchase the item. The dictionary items has been updated into a table format with the above adjustments and that table contains 5 columns. Here we have used jinja2 template here for displaying items through a forloop and then used item as variable or an object to display the items in a page.

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ def about_page():
 @app.route('/')
 def home_page():
     return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+  items = [
+      {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+      {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+      {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+  ]
+  return render_template('market.html', items = items)
   
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080, debug=True)
