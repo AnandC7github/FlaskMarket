c75881c67e3566febfe5210927fca5af21e23764
AnandC7github <122742595+AnandC7github@users.noreply.github.com>
2024-01-23 09:51:17 +0000

    now, we have modified the stock items displayed in '/market' section of the program to displaying the items which are inside the database 'market.db'.

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ class Item(db.Model):
   barcode = db.Column(db.String(length=12), nullable=False, unique=True)
   description = db.Column(db.String(length=1824), nullable=False, unique=True)
 
+  def __repr__(self):
+    return f'Item{self.name}'
+
 
 @app.route('/about/<username>')
 def about_user(username):
@@ -32,22 +35,7 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-  items = [{
-      'id': 1,
-      'name': 'Phone',
-      'barcode': '893212299897',
-      'price': 500
-  }, {
-      'id': 2,
-      'name': 'Laptop',
-      'barcode': '123985473165',
-      'price': 900
-  }, {
-      'id': 3,
-      'name': 'Keyboard',
-      'barcode': '231985128446',
-      'price': 150
-  }]
+  items = Item.query.all()
   return render_template('market.html', items=items)
   
 @app.route('/depict')
