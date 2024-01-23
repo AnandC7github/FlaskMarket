f03705ca80cf6160f959f28a8dccd86adbf9802d
AnandC7github <122742595+AnandC7github@users.noreply.github.com>
2024-01-23 06:26:54 +0000

    Certain changes made like implementing the database codes and then trying to create the database. Also added gitignore to the files required.

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
-  
-  
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1824), nullable=False, unique=True)
+
 @app.route('/about/<username>')
 def about_user(username):
-      return f'<h1>This is about the page of {username}</h1>'
-  
+    return f'<h1>This is about the page of {username}</h1>'
+
 @app.route('/about')
 def about_page():
     return '<h1>About Page</h1>'
@@ -16,12 +27,14 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-  items = [
-      {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-      {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-      {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-  ]
-  return render_template('market.html', items = items)
-  
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template('market.html', items=items)
+
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=8080, debug=True)
